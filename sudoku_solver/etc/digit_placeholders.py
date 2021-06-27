0055d764951de76ddf1c3dcd7aa3bd4ddd01e8d3
JamesdeLeon <jdeleonphys@gmail.com>
2021-06-27 17:01:35 -0500

    init commit; extra file ideas

---
+++ sudoku_solver/etc/digit_placeholders.py
@@ -0,0 +1,106 @@
+import tkinter as tk
+
+class Digit_Placeholders:
+
+	root = tk.Tk()
+
+	board_index0 = tk.IntVar()
+	board_index1 = tk.IntVar()
+	board_index2 = tk.IntVar()
+	board_index3 = tk.IntVar()
+	board_index4 = tk.IntVar()
+	board_index5 = tk.IntVar()
+	board_index6 = tk.IntVar()
+	board_index7 = tk.IntVar()
+	board_index8 = tk.IntVar()
+
+	board_index9 = tk.IntVar()
+	board_index10 = tk.IntVar()
+	board_index11 = tk.IntVar()
+	board_index12 = tk.IntVar()
+	board_index13 = tk.IntVar()
+	board_index14 = tk.IntVar()
+	board_index15 = tk.IntVar()
+	board_index16 = tk.IntVar()
+	board_index17 = tk.IntVar()
+
+	board_index18 = tk.IntVar()
+	board_index19 = tk.IntVar()
+	board_index20 = tk.IntVar()
+	board_index21 = tk.IntVar()
+	board_index22 = tk.IntVar()
+	board_index23 = tk.IntVar()
+	board_index24 = tk.IntVar()
+	board_index25 = tk.IntVar()
+	board_index26 = tk.IntVar()
+
+	board_index27 = tk.IntVar()
+	board_index28 = tk.IntVar()
+	board_index29 = tk.IntVar()
+	board_index30 = tk.IntVar()
+	board_index31 = tk.IntVar()
+	board_index32 = tk.IntVar()
+	board_index33 = tk.IntVar()
+	board_index34 = tk.IntVar()
+	board_index35 = tk.IntVar()
+
+	board_index36 = tk.IntVar()
+	board_index37 = tk.IntVar()
+	board_index38 = tk.IntVar()
+	board_index39 = tk.IntVar()
+	board_index40 = tk.IntVar()
+	board_index41 = tk.IntVar()
+	board_index42 = tk.IntVar()
+	board_index43 = tk.IntVar()
+	board_index44 = tk.IntVar()
+
+	board_index45 = tk.IntVar()
+	board_index46 = tk.IntVar()
+	board_index47 = tk.IntVar()
+	board_index48 = tk.IntVar()
+	board_index49 = tk.IntVar()
+	board_index50 = tk.IntVar()
+	board_index51 = tk.IntVar()
+	board_index52 = tk.IntVar()
+	board_index53 = tk.IntVar()
+
+	board_index54 = tk.IntVar()
+	board_index55 = tk.IntVar()
+	board_index56 = tk.IntVar()
+	board_index57 = tk.IntVar()
+	board_index58 = tk.IntVar()
+	board_index59 = tk.IntVar()
+	board_index60 = tk.IntVar()
+	board_index61 = tk.IntVar()
+	board_index62 = tk.IntVar()
+
+	board_index63 = tk.IntVar()
+	board_index64 = tk.IntVar()
+	board_index65 = tk.IntVar()
+	board_index66 = tk.IntVar()
+	board_index67 = tk.IntVar()
+	board_index68 = tk.IntVar()
+	board_index69 = tk.IntVar()
+	board_index70 = tk.IntVar()
+	board_index71 = tk.IntVar()
+
+	board_index72 = tk.IntVar()
+	board_index73 = tk.IntVar()
+	board_index74 = tk.IntVar()
+	board_index75 = tk.IntVar()
+	board_index76 = tk.IntVar()
+	board_index77 = tk.IntVar()
+	board_index78 = tk.IntVar()
+	board_index79 = tk.IntVar()
+	board_index80 = tk.IntVar()
+
+	board_in = [board_index0, board_index1, board_index2, board_index3, board_index4, board_index5, board_index6, board_index7, board_index8,
+					board_index9, board_index10, board_index11, board_index12, board_index13, board_index14, board_index15, board_index16, board_index17,
+					board_index18, board_index19, board_index20, board_index21, board_index22, board_index23, board_index24, board_index25, board_index26,
+					board_index27, board_index28, board_index29, board_index30, board_index31, board_index32, board_index33, board_index34, board_index35,
+					board_index36, board_index37, board_index38, board_index39, board_index40, board_index41, board_index42, board_index43, board_index44,
+					board_index45, board_index46, board_index47, board_index48, board_index49, board_index50, board_index51, board_index52, board_index53,
+					board_index54, board_index55, board_index56, board_index57, board_index58, board_index59, board_index60, board_index61, board_index62,
+					board_index63, board_index64, board_index65, board_index66, board_index67, board_index68, board_index69, board_index70, board_index71,
+					board_index72, board_index73, board_index74, board_index75, board_index76, board_index77, board_index78, board_index79, board_index80]
+
